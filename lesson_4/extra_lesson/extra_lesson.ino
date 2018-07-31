e9052172b127917e16d48ff655404ab5948b7418
Miguel Piedrafita <github@miguelpiedrafita.com>
2018-07-31 16:57:57 +0100

    Extra lesson cause I'm bored

---
+++ lesson_4/extra_lesson/extra_lesson.ino
@@ -0,0 +1,163 @@
+const int tonePin = 6;
+char *song = "Python:d=8,o=5,b=180:d#6,d6,4c6,b,4a#,a,4g#,g,f,g,g#,4g,f,2a#,p,a#,g,p,g,g,f#,g,d#6,p,a#,a#,p,g,g#,p,g#,g#,p,a#,2c6,p,g#,f,p,f,f,e,f,d6,p,c6,c6,p,g#,g,p,g,g,p,g#,2a#,p,a#,g,p,g,g,f#,g,g6,p,d#6,d#6,p,a#,a,p,f6,f6,p,f6,2f6,p,d#6,4d6,f6,f6,e6,f6,4c6,f6,f6,e6,f6,a#,p,a,a#,p,a,2a#";
+#define OCTAVE_OFFSET 0
+
+int notes[] = { 0,
+262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494,
+523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988,
+1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976,
+2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951
+};
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+#define isdigit(n) (n >= '0' && n <= '9')
+
+void play_rtttl(char *p)
+{
+  byte default_dur = 4;
+  byte default_oct = 6;
+  int bpm = 63;
+  int num;
+  long wholenote;
+  long duration;
+  byte note;
+  byte scale;
+  // format: d=N,o=N,b=NNN:
+  while(*p != ':') p++;
+  p++;
+  if(*p == 'd')
+  {
+    p++; p++;
+    num = 0;
+    while(isdigit(*p))
+    {
+      num = (num * 10) + (*p++ - '0');
+    }
+    if(num > 0) default_dur = num;
+    p++;
+  }
+
+  Serial.print("ddur: ");
+  Serial.println(default_dur, 10);
+  if(*p == 'o')
+  {
+    p++; p++;
+    num = *p++ - '0';
+    if(num >= 3 && num <=7) default_oct = num;
+    p++;
+  }
+  Serial.print("doct: ");
+  Serial.println(default_oct, 10);
+  if(*p == 'b')
+  {
+    p++; p++;
+    num = 0;
+    while(isdigit(*p))
+    {
+      num = (num * 10) + (*p++ - '0');
+    }
+    bpm = num;
+    p++;
+  }
+
+  Serial.print("bpm: ");
+  Serial.println(bpm, 10);
+  wholenote = (60 * 1000L / bpm) * 4;
+  Serial.print("whole note: ");
+  Serial.println(wholenote, 10);
+  while(*p)
+  {
+    num = 0;
+    while(isdigit(*p))
+    {
+      num = (num * 10) + (*p++ - '0');
+    }
+    
+    if(num) duration = wholenote / num;
+    else duration = wholenote / default_dur;
+    note = 0;
+
+    switch(*p)
+    {
+      case 'c':
+        note = 1;
+        break;
+      case 'd':
+        note = 3;
+        break;
+      case 'e':
+        note = 5;
+        break;
+      case 'f':
+        note = 6;
+        break;
+      case 'g':
+        note = 8;
+        break;
+      case 'a':
+        note = 10;
+        break;
+      case 'b':
+        note = 12;
+        break;
+      case 'p':
+      default:
+        note = 0;
+    }
+    p++;
+    if(*p == '#')
+    {
+      note++;
+      p++;
+    }
+    if(*p == '.')
+    {
+      duration += duration/2;
+      p++;
+    }
+    if(isdigit(*p))
+    {
+      scale = *p - '0';
+      p++;
+    }
+    else
+    {
+      scale = default_oct;
+    }
+
+    scale += OCTAVE_OFFSET;
+
+    if(*p == ',')
+      p++;
+    if(note)
+    {
+      Serial.print("Playing: ");
+      Serial.print(scale, 10); Serial.print(' ');
+      Serial.print(note, 10); Serial.print(" (");
+      Serial.print(notes[(scale - 4) * 12 + note], 10);
+      Serial.print(") ");
+      Serial.println(duration, 10);
+      tone(tonePin, notes[(scale - 4) * 12 + note]);
+      delay(duration);
+      noTone(tonePin);
+    }
+    else
+    {
+      Serial.print("Pausing: ");
+      Serial.println(duration, 10);
+      delay(duration);
+    }
+  }
+}
+
+void loop()
+{
+    play_rtttl(song);
+    Serial.println("Done playing");
+    delay(100000);
+    
+}
